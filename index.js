85a0551ce7aafa3d484e2cf2c78294dbe0735ca4
Suriya-K7 <usraising@gmail.com>
2023-08-23 21:38:35 +0530

    minor updates

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-// app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // File storage
 
@@ -51,7 +51,6 @@ const upload = multer({ storage });
 
 app.post("/auth/register", upload.single("picture"), register);
 app.post("/posts", verifyToken, upload.single("picture"), createPost);
-app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 // Routes
 app.use("/auth", authRoutes);
